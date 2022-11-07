3e75ecb62bbbba02ca40c8d41bad08ac87c48b1d
Mina0007 <eng.minanabil20@gmail.com>
2022-11-07 11:37:55 +0200

    New Project

---
+++ src/components/DatesAction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Row, Col, Button } from 'react-bootstrap'
+const DatesAction = ({deleteData,onViewData}) => {
+    return (
+        <Row className="justify-content-center my-2 ">
+          <Col sm="8" className="d-flex justify-content-between">
+            <Button onClick={deleteData} className="btn-style p-2">مسح الكل</Button>
+            <Button onClick={onViewData} className="btn-style p-2">عرض البيانات</Button>
+          </Col>
+        </Row>
+    )
+}
+
+export default DatesAction
